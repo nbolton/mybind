c6d32a2a653f1cef047945b53b300951ad71a733
nick <nick@5305bdc9-7064-4aa5-bdc8-4d6555d1b6d1>
2012-12-09 17:18:07 +0000

    using legacy database column names.

--- trunk/web/php/MyBind/DataStores/DnsRecordDataStore.php
+++ trunk/web/php/MyBind/DataStores/DnsRecordDataStore.php
@@ -14,10 +14,13 @@ class DnsRecordDataStore extends DataStore {
 
   public function getByZoneId($zoneId) {
     $result = $this->query(
-      "select * from dnsrecord ".
-      "where zoneId = %d ".
+      "select id, zone_id as zoneId, ".
+      "name, type, ttl, aux, data ".
+      "from mybindweb_dnsrecord ".
+      "where zone_id = %d ".
       "order by type, name, aux, `data`",
       $zoneId);
+    
     return $this->fromResult($result);
   }
 }
